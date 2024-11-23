d99530d073537f3051d76422dd0025b0eb316847
Denys Kelba <98632389+Mate021724e@users.noreply.github.com>
2024-11-23 12:09:43 +0200

    fixed conflict

--- Kelba/Lab-4:module/movies/utils/categories.py
+++
@@ -1,9 +0,0 @@
-from movies.models import Category
-
-def get_all_categories():
-    """
-    Отримує всі категорії з бази даних.
-    :return: QuerySet з усіма категоріями.
-    """
-    categories = Category.objects.all()
-    return categories
